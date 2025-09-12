3ede53d83a87bab0b1d2aa6fa435a0090063a10c
Roma Slugin <romaslugin@gmail.com>
2025-09-12 17:26:15 +0300

    first att. fix filter pag search sort main data

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -6,37 +6,80 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
     pages.firstElementChild.remove();
 
     return (data, state, action) => {
-        // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
-        const rowsPerPage = state.rowsPerPage;
-        const pageCount = Math.ceil(data.length / rowsPerPage);
-        let page = state.page;
+        // шаг 2 пр7.
+        let pageCount;
 
-        // @todo: #2.6 — обработать действия
+        const applyPagination = (query, state, action) => {
+            const limit = state.rowsPerPage;
+            let page = state.page;
         
-        if(action) switch(action.name) {
-            case 'prev': page = Math.max(1, page - 1);
-            break;            // переход на предыдущую страницу
-            case 'next': page = Math.min(pageCount, page + 1); break;    // переход на следующую страницу
-            case 'first': page = 1; break;                                // переход на первую страницу
-            case 'last': page = pageCount; break;                        // переход на последнюю страницу
+            if(action) switch(action.name) {
+                case 'prev': page = Math.max(1, page - 1);
+                break;            // переход на предыдущую страницу
+                case 'next': page = Math.min(pageCount, page + 1); break;    // переход на следующую страницу
+                case 'first': page = 1; break;                                // переход на первую страницу
+                case 'last': page = pageCount; break;                        // переход на последнюю страницу
+            }
+
+            return Object.assign({}, query, { // добавим параметры к query, но не изменяем исходный объект
+                limit,
+                page
+            });
         }
 
-        // @todo: #2.4 — получить список видимых страниц и вывести их
+        const updatePagination = (total, {page, limit}) => {
+            pageCount = Math.ceil(total / limit);
 
-        const visiblePages = getPages(page, pageCount, 5);
-
-        pages.replaceChildren(...visiblePages.map((pageNumber) => {
-            const el = pageTemplate.cloneNode(true);
-            return createPage(el, pageNumber, pageNumber === page);
-        }));
+            const visiblePages = getPages(page, pageCount, 5);
+            pages.replaceChildren(
+                ...visiblePages.map((pageNumber) => {
+                    const el = pageTemplate.cloneNode(true);
+                    return createPage(el, pageNumber, pageNumber === page);
+                })
+            );
+            fromRow.textContent = (page - 1) * limit + 1;                    // С какой строки выводим
+            toRow.textContent = Math.min((page * limit), total);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+            totalRows.textContent = total;                                // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
         
-        // @todo: #2.5 — обновить статус пагинации
-        fromRow.textContent = (page - 1) * rowsPerPage + 1;                    // С какой строки выводим
-        toRow.textContent = Math.min((page * rowsPerPage), data.length);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
-        totalRows.textContent = data.length;                                // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
+        }   
         
-        // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        const skip = (page - 1) * rowsPerPage;
-        return data.slice(skip, skip + rowsPerPage);
+        return {
+            updatePagination,
+            applyPagination
+        }
     }
 }
+
+        
+        // // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
+        // const rowsPerPage = state.rowsPerPage;
+        // const pageCount = Math.ceil(data.length / rowsPerPage);
+        // let page = state.page;
+
+        // // @todo: #2.6 — обработать действия
+        
+        // if(action) switch(action.name) {
+        //     case 'prev': page = Math.max(1, page - 1);
+        //     break;            // переход на предыдущую страницу
+        //     case 'next': page = Math.min(pageCount, page + 1); break;    // переход на следующую страницу
+        //     case 'first': page = 1; break;                                // переход на первую страницу
+        //     case 'last': page = pageCount; break;                        // переход на последнюю страницу
+        // }
+
+        // // @todo: #2.4 — получить список видимых страниц и вывести их
+
+        // const visiblePages = getPages(page, pageCount, 5);
+
+        // pages.replaceChildren(...visiblePages.map((pageNumber) => {
+        //     const el = pageTemplate.cloneNode(true);
+        //     return createPage(el, pageNumber, pageNumber === page);
+        // }));
+        
+        // // @todo: #2.5 — обновить статус пагинации
+        // fromRow.textContent = (page - 1) * rowsPerPage + 1;                    // С какой строки выводим
+        // toRow.textContent = Math.min((page * rowsPerPage), data.length);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+        // totalRows.textContent = data.length;                                // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
+        
+        // // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
+        // const skip = (page - 1) * rowsPerPage;
+        // return data.slice(skip, skip + rowsPerPage);
--- src/main.js
+++ src/main.js
@@ -4,20 +4,24 @@ import './style.css'
 import {data as sourceData} from "./data/dataset_1.js";
 
 import {initData} from "./data.js";
+
 import {processFormData} from "./lib/utils.js";
+
+import {initSearching} from "./components/searching.js";
 
 import {initSorting} from "./components/sorting.js";
 
 import {initPagination} from "./components/pagination.js";
 
 import {initTable} from "./components/table.js";
+
+import { initFiltering } from './components/filtering.js';
 // import { act } from 'react';
 // @todo: подключение
 
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
-
+const api = initData(sourceData);
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
@@ -37,15 +41,24 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-
-function render(action) {
+// шаг 1 пр7.
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let query = {}; // параметры для api
     // @todo: использование
-    result = applySorting(result, state, action);
-    result = applyPagination(result, state, action);
+    // шаг 0 пр7. 
+    // result = applySorting(result, state, action);
+    // result = applyPagination(result, state, action);
+    query = applyPagination(query, state, action); // обновляем query
+    query = applyFiltering(query, state, action); // обновляем query
+    query = applySearching(query, state, action); // обновляем query
+    query = applySorting(query, state, action); // обновляем query
 
-    sampleTable.render(result)
+    const {total, items} = await api.getRecords(query);
+
+    updatePagination(total, query); // обновляем пагинацию
+
+    sampleTable.render(items)
 }
 
 const sampleTable = initTable({
@@ -56,24 +69,58 @@ const sampleTable = initTable({
 }, render);
 
 // @todo: инициализация
+const applySearching = initSearching(search.name)
+
+// шаг 0 пр7.
+// const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
+//     searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
+// });
+
+const {applyFiltering, updateIndexes} = initFiltering(sampleTable.filter.elements); // передаём элементы фильтра
+
 const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
     sampleTable.header.elements.sortByDate,
     sampleTable.header.elements.sortByTotal
 ]); 
 
-const applyPagination = initPagination(
-    sampleTable.pagination.elements,
-    (el, page, isCurrent) => {
-        const input = el.querySelector('input');
-        const label = el.querySelector('span');
-        input.value = page;
-        input.checked = isCurrent;
-        label.textContent = page;
-        return el;
-    }
-)
+// const applyPagination = initPagination(
+//     sampleTable.pagination.elements,
+//     (el, page, isCurrent) => {
+//         const input = el.querySelector('input');
+//         const label = el.querySelector('span');
+//         input.value = page;
+//         input.checked = isCurrent;
+//         label.textContent = page;
+//         return el;
+//     }
+// )
+// вариант в шаге 2 пр7. не работал
+// const { applyPagination, updatePagination } = initPagination(...);
+
+// исправление через доп объявление функции
+const getPagination = initPagination(
+  sampleTable.pagination.elements,
+  (el, page, isCurrent) => {
+    const input = el.querySelector('input');
+    const label = el.querySelector('span');
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  }
+);
+const { applyPagination, updatePagination } = getPagination(); 
+
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+    })
+}
+
+init().then(render);
